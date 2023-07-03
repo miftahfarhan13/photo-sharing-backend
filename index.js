abb21e894f2e70d0c56c31668f9c59ad8d609e74
Miftah Muhammad Farhan <miftahfarhan20@gmail.com>
2023-07-03 12:09:21 +0700

    feat: new upload

--- index.js
+++ index.js
@@ -6,35 +6,21 @@ import FollowRoute from "./src/routes/FollowRoute.js"
 import CommentRoute from "./src/routes/CommentRoute.js"
 import LikeRoute from "./src/routes/LikeRoute.js"
 import ImageRoute from "./src/routes/ImageRoute.js"
-import multer from "multer";
 import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().getTime() + '-' + file.originalname)
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
-
 app.use(bodyParser.json())
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
+
+app.use(
+    fileUpload({
+        abortOnLimit: true,
+    })
+);
 
 app.use(UserRoute)
 app.use(PostRoute)
